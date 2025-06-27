9076a0494fba5bc9c6e3d3d1e9d73d30a433bf1d
Ryan Jensen <rj1044@princeton.edu>
2025-06-27 13:51:02 -0400

    Add matching recipe identification functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get "recipes/list"
+  get "ingredient/index"
+  get "ingredient/show"
+  get "ingredient/new"
+  get "ingredient/create"
   get "home/index"
   devise_for :users
+  resources :recipes, :ingredients
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
